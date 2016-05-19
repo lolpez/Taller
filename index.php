c151b346f9b2baf40100405d766419da832f87fc
lulax666 <luiyicpu@hotmail.com>
2016-05-19 11:31:22 -0400

    mongotest

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
-<form method="POST" enctype="multipart/form-data" action="upload.php">
-    <label for="username">Nombre archivo:</label>
-    <input type="text" name="nombre"/>
-    <label for="pic">Archivo: </label>
-    <input type="file" name="archivo"/>
-    <input type="submit"/>
-</form>
+<?php
+    header('Location: mongo');
+?>
---
+++ mongo/index.php
@@ -0,0 +1,7 @@
+<form method="POST" enctype="multipart/form-data" action="../upload.php">
+    <label for="username">Nombre archivo:</label>
+    <input type="text" name="nombre"/>
+    <label for="pic">Archivo: </label>
+    <input type="file" name="archivo"/>
+    <input type="submit"/>
+</form>
