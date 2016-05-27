07dfb63ced21e6f7e02250300d768e8e5695d37d
lulax666 <luiyicpu@hotmail.com>
2016-05-26 20:10:21 -0400

    agg tmr

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
 <?php
 include_once 'model/singleton/mysql.php';
-try {
-    $this->pdo = ConexionMysql::getInstance()->obtenerConexion();
-} catch (Exception $e) {
-    die($e->getMessage());
-}
+$con = ConexionMysql::getInstance()->obtenerConexion();
 ?>
