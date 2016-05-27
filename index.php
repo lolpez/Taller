54f7199d4952cd169157d22343db458f6f0cab61
lulax666 <luiyicpu@hotmail.com>
2016-05-26 20:14:37 -0400

    con tmr

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+include_once 'model/singleton/mongo.php';
 include_once 'model/singleton/mysql.php';
 $con = ConexionMysql::getInstance()->obtenerConexion();
+$con2 = ConexionMongo::getInstance()->obtenerConexion();
 ?>
